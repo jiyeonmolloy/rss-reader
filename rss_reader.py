d80e419c76bd6dc06fa07a6d6ccdadcd18c6dea3
Ji <jjeonie@gmail.com>
2025-02-09 21:33:49 +1300

    Set version of python on workflows

--- rss_reader.py
+++ rss_reader.py
@@ -36,6 +36,7 @@ def send_to_discord(message):
         "content": message
     }
     response = requests.post(webhook_url, json=payload)
+    
     if response.status_code == 204 or response.status_code == 200:
         print("Message sent successfully to Discord! Woohoo!")
     else:
